a2464fabacdc64ccd44a334486b7dc44e6619d01
John Holmes <johnh@Johns-MacBook-Pro.local>
2017-10-15 17:54:25 -0600

    working show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/support'
-  get 'users/new'
+  get '/support', to: 'static_pages#support'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
