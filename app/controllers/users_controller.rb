f3002a80c7490ef65baefb49ca658215aae960de
John Holmes <johnh@getyardstick.com>
2018-01-31 21:22:35 -0700

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -15,6 +16,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Account Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -24,6 +39,13 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
     end
 
 end
